942b07d53ddf821fd77a0d0299908f6910c5b687
marchegiani92 <majkicmarko29@yahoo.com>
2017-06-05 13:08:19 +0200

    initial commit

---
+++ src/components/Knjige.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="knjige">
+  test
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'knjige',
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
